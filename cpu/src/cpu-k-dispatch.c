f2b0267e49ddd895e041ff9c3a698619fbe21e87
ViniBitHub <facundo@vinitzca.com.ar>
2025-05-04 21:49:15 +0000

    #38 envio syscall IO desde CPU dispatch

--- cpu/src/cpu-k-dispatch.c
+++ cpu/src/cpu-k-dispatch.c
@@ -14,26 +14,74 @@ int manejar_conexion_kernel_dispatch(){
 				break;
 				
 			case INFO_PROC_EXEC:
+				
 				recv(fd_conexion_kernel_dispatch, &(paquete->buffer->size), sizeof(uint32_t), 0);
 				paquete->buffer->stream = malloc(paquete->buffer->size);
 				recv(fd_conexion_kernel_dispatch, paquete->buffer->stream, paquete->buffer->size, 0);
-				log_trace(logger_cpu, "llegue desde Kernel");
+				t_peticion_instruccion* infoPCB = deserializar_peticion_instruccion(paquete->buffer->stream);
+				log_trace(logger_cpu, "PID: %d | PC: %d", infoPCB->pid, infoPCB->pc);
+
+				// 
+
+				// El proceso debe realizar una IO ahora:
+				t_param_io* pruebaIO = malloc(sizeof(t_param_io));
+				pruebaIO->dispositivo = "MOUSE";
+				pruebaIO->dispositivo_length = string_length(pruebaIO->dispositivo);
+				pruebaIO->tiempo = 25000;
+				enviar_io_kernel(*pruebaIO, fd_conexion_kernel_dispatch);
+				
 				break;
 
-		case INSTRUCCION:
-			break;
+			case INSTRUCCION:
+				break;
 			
-		case -1:
-			log_error(logger_cpu, "el cliente se desconecto.");
-			return EXIT_FAILURE;
-		default:
-			log_warning(logger_cpu, "Operacion desconocida. No quieras meter la pata");
-			break;
+			case -1:
+				log_error(logger_cpu, "el cliente se desconecto.");
+				return EXIT_FAILURE;
+			default:
+				log_warning(logger_cpu, "Operacion desconocida. No quieras meter la pata");
+				break;
+			}
 		}
-	}
-
 	close(fd_conexion_kernel_dispatch);
 	return EXIT_SUCCESS;
 }
 
+t_peticion_instruccion* deserializar_info_pcb(t_buffer* buffer) {
+	t_peticion_instruccion* infoPCB = malloc(sizeof(t_peticion_instruccion));
+    void* stream = buffer->stream;
 
+    memcpy(&(infoPCB->pid), stream, sizeof(int)); stream += sizeof(int);
+    memcpy(&(infoPCB->pc), stream, sizeof(int)); stream += sizeof(int);
+    
+    return infoPCB;
+
+}
+
+void enviar_io_kernel(t_param_io pruebaIO, int socket_cliente){
+	t_buffer* buffer = malloc(sizeof(t_buffer));
+    buffer->size = sizeof(int64_t) + sizeof(uint32_t) + (pruebaIO.dispositivo_length);
+    buffer->stream = malloc(buffer->size);
+    uint32_t offset = 0;
+
+    memcpy(buffer->stream + offset, &pruebaIO.tiempo, sizeof(int64_t)); offset += sizeof(int64_t);
+    memcpy(buffer->stream + offset, &pruebaIO.dispositivo_length, sizeof(uint32_t)); offset += sizeof(uint32_t);
+    memcpy(buffer->stream + offset, pruebaIO.dispositivo, pruebaIO.dispositivo_length);
+
+    t_paquete* paquete = malloc(sizeof(t_paquete));
+    paquete->codigo_operacion = SYSCALL_IO;
+    paquete->buffer = buffer;
+    void* a_enviar = malloc(buffer->size + sizeof(int) + sizeof(uint32_t));
+    offset = 0;
+
+    memcpy(a_enviar + offset, &(paquete->codigo_operacion), sizeof(int)); offset += sizeof(int);
+    memcpy(a_enviar + offset, &(paquete->buffer->size), sizeof(uint32_t)); offset += sizeof(uint32_t);
+    memcpy(a_enviar + offset, paquete->buffer->stream, paquete->buffer->size);
+    send(socket_cliente, a_enviar, buffer->size + sizeof(int) + sizeof(uint32_t), 0);
+
+    free(a_enviar);
+    free(paquete->buffer->stream);
+    free(paquete->buffer);
+    free(paquete);
+
+}
