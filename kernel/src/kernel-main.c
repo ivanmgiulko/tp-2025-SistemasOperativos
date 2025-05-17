7dfda4343f980c446dffe8cfc71c7d49aa7e0c79
ViniBitHub <facundo@vinitzca.com.ar>
2025-05-17 18:37:24 +0000

    #53.1 validacion del tamanio memo

--- kernel/src/kernel-main.c
+++ kernel/src/kernel-main.c
@@ -1,10 +1,15 @@
 #include "kernel-main.h"
 
 int main(int argc, char* argv[]) {
-	// if(argc <= CANT_MINIMA_ARGUMENTOS){
-	// 	fprintf(stderr, "Falta nombre Archivo de pseudocodigo y/o tamanio del proceso papu lince \n");
-	// 	return EXIT_FAILURE;
-	// }
+	if(argc <= CANT_MINIMA_ARGUMENTOS){
+		fprintf(stderr, "Falta nombre Archivo de pseudocodigo y/o tamanio del proceso papu lince \n");
+		return EXIT_FAILURE;
+	}
+
+	// argv[1] = "/home/utnso/Desktop/tp-2025-1c-FAMILIA-MATRIX/kernel/PATH_INSTRUCCIONES.txt";
+	// argv[2] = "2prueba1";
+
+	_chequear_segundo_argumento_es_numero(argv[2]);
 
 	logger_kernel = log_create("kernel.log", "log", true, LOG_LEVEL_TRACE); // Crea el logger del Kernel
 
@@ -34,12 +39,13 @@ int main(int argc, char* argv[]) {
         abort();
     }
 
-	// decir_algoritmo();
+	
 	inicializar_estructuras();
-	// Habria que contemplar que pasa si el usuario es un imbecil y no mete ni el path ni el tamanio del proceso -_-
-	// Le borramos system32 por pescado.-S
-	argv[1] = "/home/utnso/Desktop/tp-2025-1c-FAMILIA-MATRIX/kernel/PATH_INSTRUCCIONES.txt";
-	argv[2] = "4000";
+	
+	
+
+	
+
 	log_trace(logger_kernel, "Creando proceso cero con el path: %s y tamanio: %s", argv[1], argv[2]);
 	crear_proceso_cero(argv[1], atoi(argv[2]));
 	
--- kernel/src/utils_kernel/kernel-utils.h
+++ kernel/src/utils_kernel/kernel-utils.h
@@ -2,5 +2,6 @@
 #define KERNEL_UTILS_H_
 #include "kernel-gestor.h"
 
+    void _chequear_segundo_argumento_es_numero(char* numero_en_string);
 
 #endif // KERNEL_UTILS_H_
